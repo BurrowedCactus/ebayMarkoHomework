267d1b15d073d6cd05ede7ef9e465a463067d9f3
Yongyuan Bai <yongyuanbaimike@gmail.com>
2024-06-26 20:22:09 +0800

    implement countdown, features and quote form as homework requested.

---
+++ src/routes/_index/components/feature.js
@@ -0,0 +1,25 @@
+// ES Module export
+export const featureData = {
+  title: "Allstate Extended Vehicle Care",
+  subtitle:
+    "For as little as $19/mo, we'll pay for repairs after your manufacturer's warranty expires.",
+  features: [
+    {
+      icon: "src/routes/_index/logo.svg",
+      title: "Affordable Protection Plans",
+      description: "Save up to 50% when you buy online from Allstate.",
+    },
+    {
+      icon: "src/routes/_index/logo.svg",
+      title: "100% of covered repair costs",
+      description:
+        "Covers everything from your engine and transmission, down to the brakes and steering at no additional cost.",
+    },
+    {
+      icon: "src/routes/_index/logo.svg",
+      title: "Repair Anywhere",
+      description:
+        "You choose your mechanic, and we pay them directly for repairs, so you aren't stuck chasing down reimbursements.",
+    },
+  ],
+};

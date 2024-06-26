694993c2d08768e66d3e63ebd72b38e33250b62a
Yongyuan Bai <yongyuanbaimike@gmail.com>
2024-06-26 20:36:43 +0800

    update icon image alt

--- src/routes/_index/components/feature.js
+++ src/routes/_index/components/feature.js
@@ -6,17 +6,20 @@ export const featureData = {
   features: [
     {
       icon: "src/routes/_index/logo.svg",
+      iconAlt: "icon description",
       title: "Affordable Protection Plans",
       description: "Save up to 50% when you buy online from Allstate.",
     },
     {
       icon: "src/routes/_index/logo.svg",
+      iconAlt: "icon other description",
       title: "100% of covered repair costs",
       description:
         "Covers everything from your engine and transmission, down to the brakes and steering at no additional cost.",
     },
     {
       icon: "src/routes/_index/logo.svg",
+      iconAlt: "icon more description",
       title: "Repair Anywhere",
       description:
         "You choose your mechanic, and we pay them directly for repairs, so you aren't stuck chasing down reimbursements.",
